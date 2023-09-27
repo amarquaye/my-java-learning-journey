e97741e63583d5bdee2e84d247463a47de8a0622
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-09-27 12:28:32 +0000

    Updated MoMo

--- src/console_apps/MoMo/MoMo.java
+++ src/console_apps/MoMo/MoMo.java
@@ -3,5 +3,14 @@ package console_apps.MoMo;
 public class MoMo {
     public static void main(String[] args){
         System.out.println("Welcome to MoMo by Jesse Amarquaye");
+        System.out.println("Menu");
+        System.out.println("1) Transfer Money");
+        System.out.println("2) MoMoPay & Pay Bill");
+        System.out.println("3) Airtime & Bundles");
+        System.out.println("4) Allow Cash Out");
+        System.out.println("5) Financial Services");
+        System.out.println("6) My Wallet");
+        System.out.println("7) MoMo Promo");
+
     }
 }
