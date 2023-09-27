4eef9aa1fe89bcf5a27d1c820503e6e018b03e1a
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-09-27 12:54:50 +0000

    Updated MashUp

---
+++ src/console_apps/MTN_Pulse/MashUp.java
@@ -0,0 +1,14 @@
+package console_apps.MTN_Pulse;
+
+public class MashUp {
+    public static void main(String[] args){
+        System.out.println("Welcome to MTN Pulse.");
+        System.out.println("1) MashUp for Self");
+        System.out.println("2) MashUp for Others");
+        System.out.println("3) Convert Minutes To Data");
+        System.out.println("4) Pulse Loyalty");
+        System.out.println("5) Check Promo Points");
+        System.out.println("99) More");
+        System.out.println("0) Back");
+    }
+}
